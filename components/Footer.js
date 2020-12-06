466b28d3cab7bc97dc12dff6f05beb99d130dc69
Mitul Shah <mitulxshah@gmail.com>
2020-12-05 19:18:45 -0500

    footer change

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Link, Text } from "@chakra-ui/react";
 
 export default function Footer() {
     return (
-        <Box bg="white" w="full">
-            <Box maxWidth="1250px" m="0 auto" h="10vh" display="flex" alignItems="center" justifyContent="center" mt={20}>
-                This is a footer
-            </Box>
+      <Box bg="white" w="full">
+        <Box
+          maxWidth="1250px"
+          m="0 auto"
+          h="10vh"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          mt={20}
+        >
+          <Text fontSize="sm">Paprback</Text>
+          <Text fontSize="sm">
+            Built by{' '}
+            <Link href="https://twitter.com/typicalmitul" target="_blank">
+              Mitul Shah
+            </Link>{' '}
+            👋
+          </Text>
         </Box>
-    )
+      </Box>
+    );
 }
